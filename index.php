ec47c4dc05fdbf77668acbc17f138f037f831572
Salvador Castro <salvacastro06@gmail.com>
2023-07-14 18:45:53 -0300

    first files

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+include("template/cabecera.php");
+include("template/pie.php");
---
+++ template/cabecera.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD-PHP</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <ul class="nav navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">CRUD</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">INICIO</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="productos.php">LIBROS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="nosotros.php">NOSOTROS</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container">
+    <div class="row">
