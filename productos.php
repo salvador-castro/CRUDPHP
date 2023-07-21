2c4c162c2e6d62f282bcd11f7e8d714b67d4d11b
Salvador Castro <salvacastro06@gmail.com>
2023-07-21 20:07:04 -0300

    changes Adminitrador folder

--- productos.php
+++ productos.php
@@ -1,43 +1,24 @@
 <?php include("template/cabecera.php");?>
 
+<?php
+include("administrador/config/bbdd.php");
+$sentenciaSQL = $conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibro = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php foreach($listaLibro as $libro) { ?>
+
     <div class="col-md-3">
         <div class="card">
-            <img class="card-img-top" src="https://media.tenor.com/2sHsTvXPB3QAAAAC/book.gif" alt="">
+            <img class="card-img-top" src="./img/<?php echo $libro['imagen']?>" alt="">
             <div class="card-body">
-                <h4 class="card-title">Libro PHP</h4>
+                <h4 class="card-title"><?php echo $libro['nombre']?></h4>
                 <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
             </div>
         </div>
     </div>
 
-    <div class="col-md-3">
-        <div class="card">
-            <img class="card-img-top" src="https://media.tenor.com/2sHsTvXPB3QAAAAC/book.gif" alt="">
-            <div class="card-body">
-                <h4 class="card-title">Libro PHP</h4>
-                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-3">
-        <div class="card">
-            <img class="card-img-top" src="https://media.tenor.com/2sHsTvXPB3QAAAAC/book.gif" alt="">
-            <div class="card-body">
-                <h4 class="card-title">Libro PHP</h4>
-                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-3">
-        <div class="card">
-            <img class="card-img-top" src="https://media.tenor.com/2sHsTvXPB3QAAAAC/book.gif" alt="">
-            <div class="card-body">
-                <h4 class="card-title">Libro PHP</h4>
-                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-            </div>
-        </div>
-    </div>
+<?php } ?>
 
 <?php include("template/pie.php"); ?>
